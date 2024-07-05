26c1e3b7285f351b3e1079719b18f7143559c21c
rkddn0301 <rkddn0301@naver.com>
2024-07-05 17:09:44 +0900

    6.1 useEffect 	modified:   react.txt 	modified:   src/App.js 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// React.StrictMode 삭제 : console.log가 2번 찍힘
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
