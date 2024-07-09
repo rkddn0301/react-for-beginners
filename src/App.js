0f70820c42d052104da418740e54f52d076ed377
rkddn0301 <rkddn0301@naver.com>
2024-07-09 17:27:01 +0900

    7.2 Coin Tracker 	modified:   react.txt 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -3,49 +3,85 @@ import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setTodo] = useState(0);
-  const [toDos, setToDos] = useState([]);
+  const [loading, setLoading] = useState(true); // Coin API를 가져오기 전까지의 화면 state
+  const [coins, setCoins] = useState([]); // Coin API 정보가 들어갈 배열 state
 
-  const onChange = (event) => setTodo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    // ...Array : 현재 가지고 있는 전체 배열 값을 의미.
-    // currentArray 매개변수에 작성된 toDo를 currentArray 배열에 넣는다는 의미.
-    setToDos((currentArray) => [toDo, ...currentArray]);
+  const [usd, setUsd] = useState(0); // 달러 입력란 state
+  const [tracker, setTracker] = useState(0); // 달러 입력 시
 
-    setTodo("");
+  // react-DOM 페이지 접속 시 바로 coin 페이지 API 정보를 가져오도록 설정
+  useEffect(() => {
+    fetch(`https://api.coinpaprika.com/v1/tickers`).then((response) => {
+      response.json().then((json) => {
+        setCoins(json);
+        setLoading(false);
+        console.log(json);
+      });
+    });
+  }, []);
+
+  // 달러 입력 시 바로 반환되는 함수
+  const onChange = (e) => {
+    setUsd(e.target.value);
   };
 
-  // toDO 작성 시 배열인 toDos에 반영될 때마다 console.log 발생
+  // Coin 종류 선택 시 바로 반영되는 함수
+  const onSelect = (e) => {
+    // 초기값이 "xx"인데 해당사항이 아니면 선택된 것이므로 tracker 작업 진행
+    if (e.target.value !== "xx") {
+      const coinTracker = parseInt(e.target.value);
+      setTracker(coinTracker);
+    }
+    // 종류 변경 시 작성한 값 초기화
+    setUsd(0);
+  };
+
+  // Coin 전환이 제대로 되었는지 확인
   useEffect(() => {
-    console.log(toDos);
-  }, [toDos]);
+    console.log(tracker);
+  }, [tracker]);
 
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          onChange={onChange}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      {/* array.map((value, key)) : 하나의 array(toDos)에 있는 value을 내가 원하는 무엇이든지로 바꿔줌. 
-                  array에 개수만큼 반복출력해줌.*/}
-      {/* map의 인자 중에 value은 array의 대체변수이고, key는 map에서 출력할 때 필요한 순서라고 보면된다. */}
-      {/* map은 보통 `key: value` 출력 방식인데 react에서 value만 출력하면 key가 없는거라 list 형식으로
-      인식하여 출력은 되지만 오류가 발생한다. 따라서 key도 지정해주는 것이 좋다. */}
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          {/* Coin API가 삽입되어 종류를 확인하는 select 태그 */}
+          <select onChange={onSelect}>
+            {/* 초기 선택란은 value="xx" */}
+            <option value="xx">Choice</option>
+            {coins.map((coin) => (
+              <option key={coin.id} value={coin.quotes.USD.price}>
+                {coin.name} ({coin.symbol}):${parseInt(coin.quotes.USD.price)}{" "}
+                USD
+              </option>
+            ))}
+          </select>
+          <br />
+          {/* USD 달러 입력 input 태그 */}
+          <label htmlFor="caculator">USD</label>
+          <input
+            id="caculator"
+            type="number"
+            placeholder="$USD"
+            onChange={onChange}
+            value={usd}
+          />
+          <br />
+          {/* CoinTracker 반영 input 태그 */}
+          <label htmlFor="tracker">track</label>
+          <input
+            id="tracker"
+            type="number"
+            placeholder="tracker"
+            onChange={onChange}
+            value={usd / tracker}
+            disabled
+          />
+        </div>
+      )}
     </div>
   );
 }
